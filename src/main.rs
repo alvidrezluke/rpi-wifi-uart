4118d920c6cd2602af0948f3b9ff5b1456b5ef82
Luke Alvidrez <alvidrezluke@gmail.com>
2022-08-04 23:47:29 +0000

    Finished basic web server

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
-#[macro_use] extern crate rocket;
+use rocket::*;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+use rocket::fs::{FileServer, relative};
+
+// #[get("/")]
+// async fn index() -> Option<NamedFile> {
+//     let mut path = Path::new(relative!("/static")).join("index.html");
+//     NamedFile::open(path).await.ok()
+// }
+
+#[post("/start")]
+async fn start() -> &'static str {
+    "Success"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![start])
+        .mount("/", FileServer::from(relative!("/static")))
 }
